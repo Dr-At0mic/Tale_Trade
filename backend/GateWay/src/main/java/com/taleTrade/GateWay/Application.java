6c2f0d79dd0f96bfacb29bb3027d4a9a9349b641
Dr-At0mic <anandhu2suresh797@gmail.com>
2024-05-16 22:24:12 +0530

    Fixed Bugs in redis-Rate-limiter in the gateway and binded the remoteAddress in the keyreslover

--- backend/GateWay/src/main/java/com/taleTrade/GateWay/Application.java
+++ backend/GateWay/src/main/java/com/taleTrade/GateWay/Application.java
@@ -3,6 +3,9 @@ package com.taleTrade.GateWay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,5 +14,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean(name = "userRemoteAddressResolver")
+	public KeyResolver userKeyResolver() {
+		return exchange -> {
+			return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+		};
+	}
+
 
 }
--- backend/GateWay/src/main/java/com/taleTrade/GateWay/configurations/RedisConfig.java
+++ backend/GateWay/src/main/java/com/taleTrade/GateWay/configurations/RedisConfig.java
@@ -1,4 +1,13 @@
 package com.taleTrade.GateWay.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
 public class RedisConfig {
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
+    }
 }
